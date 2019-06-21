3fd97e87dda81aeab62b8884cd8fce7d68443b4d
Stéphane Cerveau <scerveau@collabora.com>
2019-06-21 18:54:47 +0200

    Add verbose mode from elements.

--- src/gstnlaunch.c
+++ src/gstnlaunch.c
@@ -34,6 +34,8 @@ typedef struct _GstScalableTranscoder
   GstState state;
   gboolean buffering;
   gboolean is_live;
+  gboolean quiet;
+  gchar **exclude_args;
 } GstScalableTranscoder;
 
 static gboolean
@@ -122,6 +124,49 @@ message_cb (GstBus * bus, GstMessage * message, gpointer user_data)
         }
         thiz->buffering = TRUE;
       }
+      break;
+    }
+    case GST_MESSAGE_PROPERTY_NOTIFY:{
+      const GValue *val;
+      const gchar *name;
+      GstObject *obj;
+      gchar *val_str = NULL;
+      gchar **ex_prop, *obj_name;
+
+      if (thiz->quiet)
+        break;
+
+      gst_message_parse_property_notify (message, &obj, &name, &val);
+
+      /* Let's not print anything for excluded properties... */
+      ex_prop = thiz->exclude_args;
+      while (ex_prop != NULL && *ex_prop != NULL) {
+        if (strcmp (name, *ex_prop) == 0)
+          break;
+        ex_prop++;
+      }
+      if (ex_prop != NULL && *ex_prop != NULL)
+        break;
+
+      obj_name = gst_object_get_path_string (GST_OBJECT (obj));
+      if (val != NULL) {
+        if (G_VALUE_HOLDS_STRING (val))
+          val_str = g_value_dup_string (val);
+        else if (G_VALUE_TYPE (val) == GST_TYPE_CAPS)
+          val_str = gst_caps_to_string (g_value_get_boxed (val));
+        else if (G_VALUE_TYPE (val) == GST_TYPE_TAG_LIST)
+          val_str = gst_tag_list_to_string (g_value_get_boxed (val));
+        else if (G_VALUE_TYPE (val) == GST_TYPE_STRUCTURE)
+          val_str = gst_structure_to_string (g_value_get_boxed (val));
+        else
+          val_str = gst_value_serialize (val);
+      } else {
+        val_str = g_strdup ("(no value)");
+      }
+
+      g_print ("%s: %s = %s\n", obj_name, name, val_str);
+      g_free (obj_name);
+      g_free (val_str);
       break;
     }
     default:
@@ -234,10 +279,14 @@ main (int argc, char **argv)
   gchar **full_branch_desc_array = NULL;
   gchar **branch;
   GstBus *bus;
+  gboolean verbose = FALSE;
+  gulong deep_notify_id = 0;
   GOptionEntry options[] = {
     {"branch", 'b', 0, G_OPTION_ARG_STRING_ARRAY, &full_branch_desc_array,
-      "Add a custom full branch with gst-launch style description", NULL}
+        "Add a custom full branch with gst-launch style description", NULL}
     ,
+    {"verbose", 'v', 0, G_OPTION_ARG_NONE, &verbose,
+        ("Output status information and property notifications"), NULL},
     {NULL}
   };
 
@@ -271,6 +320,10 @@ main (int argc, char **argv)
       goto done;
     }
   }
+  if (verbose) {
+    deep_notify_id =
+        gst_element_add_property_deep_notify_watch (thiz->pipeline, NULL, TRUE);
+  }
 
   thiz->loop = g_main_loop_new (NULL, FALSE);
   bus = gst_pipeline_get_bus (GST_PIPELINE (thiz->pipeline));
@@ -301,6 +354,8 @@ main (int argc, char **argv)
   }
 
   g_main_loop_run (thiz->loop);
+  if (deep_notify_id != 0)
+    g_signal_handler_disconnect (thiz->pipeline, deep_notify_id);
   g_print ("STOP");
   gst_element_set_state (thiz->pipeline, GST_STATE_READY);
   /* No need to see all those pad caps going to NULL etc., it's just noise */
