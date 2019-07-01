61ce04f0854fbc0831bb18a8234ca9db3c2732ca
Stéphane Cerveau <scerveau@collabora.com>
2019-07-01 11:21:46 +0200

    fix various example scripts

--- examples/multi_branch.sh
+++ examples/multi_branch.sh
@@ -7,18 +7,18 @@ INSTANCES=$2
 FILE_ELEMENT="filesrc location=$FILEPATH"
 DECODE_BRANCH=" ! decodebin"
 CAPS_FILTER=
+QUEUE="! queue max-size-bytes=0"
 ENC_ELEMENT=" ! avenc_mpeg2video"
-DEC_ELEMENT=" ! avdec_mpeg2video"
-SINK_ELEMENT=" ! autovideosink"
-BRANCH1="$FILE_ELEMENT $CAPS_FILTER $DECODE_BRANCH $ENC_ELEMENT $DEC_ELEMENT $SINK_ELEMENT"
+DEC_ELEMENT="! h264parse ! avdec_h264"
+SINK_ELEMENT="! h264parse ! autovideosink"
+BRANCH1="$FILE_ELEMENT $QUEUE $CAPS_FILTER $DEC_ELEMENT $ENC_ELEMENT"
 #BRANCH2="$FILE_ELEMENT $CAPS_FILTER $DECODE_BRANCH $ENC_ELEMENT $DEC_ELEMENT $SINK_ELEMENT"
 
-BRANCH3="videotestsrc ! fakesink"
 
 CMD="./gst-n-launch-1.0"
 for i in `/usr/bin/seq 1 $INSTANCES`
 do
-  CMD+=" -b \"$BRANCH1\""
+  CMD+=" -b \"$BRANCH1 ! filesink location=output_${i}.mpeg2\""
 done
 
 echo $CMD
